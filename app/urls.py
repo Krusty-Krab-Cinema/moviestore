fca53939e68db50dd78d39bfa61b810bb0e69b15
pengze <769546389@qq.com>
2019-09-21 17:12:00 +0800

    join vip

--- app/urls.py
+++ app/urls.py
@@ -13,6 +13,7 @@ urlpatterns = [
     url(r'^login/$', views.login),
     url(r'^register/$', views.register),
     url(r'^quit/$', views.quit),
+    url(r'^vip/',views.vip,name='vip'),
     url(r'^person/$', views.person)
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
