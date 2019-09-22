dade2ff7a00114884fc50fb2b391e279ede5eb2d
845004942@qq.com <845004942@qq.com>
2019-09-22 11:11:10 +0800

    chen-yang

--- storeproject1/storeproject/store/models.py
+++ storeproject1/storeproject/store/models.py
@@ -1,4 +1,6 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+
 
 # Create your models here.
 
@@ -11,6 +13,7 @@ class StyleType(models.Model):
 
     def __str__(self):
         return self.style_type
+
 
 # 演员模型
 class LeadRole(models.Model):
@@ -33,6 +36,7 @@ class Country(models.Model):
     def __str__(self):
         return self.country
 
+
 # 用户模型
 class User(models.Model):
     username = models.CharField('昵称', max_length=32, unique=True)  # 用户昵称
@@ -41,11 +45,11 @@ class User(models.Model):
     subscribe = models.CharField('是否订阅电子杂志', max_length=4, default='on')  # 用户是否订阅杂志on/off
     token = models.CharField(max_length=250, default='')
 
-    is_vip = models.IntegerField('vip用户',default=0) #   默认普通用户,1vip用户.
+    is_vip = models.IntegerField('vip用户', default=0)  # 默认普通用户,1vip用户.
     is_superuser = models.IntegerField('管理员', default=0)  # 默认普通用户,1管理员用户.
-    v_start=models.DateField(default='2019-01-01')
+    v_start = models.DateField(default='2019-01-01')
 
-    v_end=models.DateField(default='2019-01-01')
+    v_end = models.DateField(default='2019-01-01')
 
     # 创建用户
     @classmethod
@@ -68,7 +72,7 @@ class Movie(models.Model):
     director = models.CharField('导演', max_length=200)  # 导演
     # lead_role = models.CharField(max_length=400)  # 主演
     length = models.CharField('片长', max_length=4)  # 片长
-    imdb_link = models.CharField('Imdb链接', max_length=200,null=True)  # Imdb链接
+    imdb_link = models.CharField('Imdb链接', max_length=200, null=True)  # Imdb链接
     mark = models.FloatField('评分', max_length=4)  # 评分
     cover_link = models.ImageField('封面图片')  # 封面图片地址
     summary = models.TextField('剧情简介')  # 剧情简介
@@ -76,18 +80,18 @@ class Movie(models.Model):
     is_carousel = models.BooleanField('是否首页轮播展示', default=0)  # 是否首页轮播图展示，默认False
     is_sidebar = models.BooleanField('侧边栏推荐展示', default=0)  # 是否侧边栏推荐展示，默认False
     country = models.ForeignKey(Country, on_delete=models.CASCADE, verbose_name='国家/地区')  # 外键关联
-    is_vipfilm = models.IntegerField('是否是vip电影',default=0) #  vip观看
+    is_vipfilm = models.IntegerField('是否是vip电影', default=0)  # vip观看
 
     style_type = models.ManyToManyField(StyleType, verbose_name='风格类型')  # 多对多关联
     lead_role = models.ManyToManyField(LeadRole, verbose_name='主演')  # 多对多关联
     like = models.ManyToManyField(User, verbose_name='喜欢')  # 喜欢/收藏
-
 
     class Meta:
         db_table = 'movies'
 
     def __str__(self):
         return self.name
+
 
 # 浏览记录
 class Visited(models.Model):
@@ -96,6 +100,7 @@ class Visited(models.Model):
 
     class Meta:
         db_table = 'visited'
+
 
 # 评论表
 class Comment(models.Model):
--- storeproject1/storeproject/store/urls.py
+++ storeproject1/storeproject/store/urls.py
@@ -19,28 +19,29 @@ from django.contrib import admin
 from store import views
 
 urlpatterns = [
-    url(r'^$', views.logina,name='logina'),
+    url(r'^$', views.userlogin,name='login'),
+    url(r'^login/$', views.userlogin,name='login'),
     url(r'^/', views.index,name='index'),
-    url(r'^pinpai/', views.pinpai, name='pinpai'),
-    url(r'^newsType/', views.newsType, name='newsType'),
+    # url(r'^pinpai/', views.pinpai, name='pinpai'),
+    # url(r'^newsType/', views.newsType, name='newsType'),
     url(r'^users/', views.users, name='users'),
-    url(r'^link/', views.link, name='link'),
+    # url(r'^link/', views.link, name='link'),
     #首页
-    url(r'^index/', views.index, name='index'),
+    url(r'^main/', views.main, name='main'),
     #影片
-    url(r'^user/', views.user, name='user'),
+    url(r'^user/(\d+)', views.user, name='user'),
     #广告页面
-    url(r'^banner/', views.banner, name='banner'),
+    url(r'^banner/(\d+)', views.banner, name='banner'),
     #评论管理
-    url(r'^opinion/', views.opinion, name='opinion'),
+    url(r'^opinion/(\d+)', views.opinion, name='opinion'),
     #管理员编辑
-    url(r'^manager/', views.manager, name='manager'),
+    url(r'^manager/(\d+)', views.manager, name='manager'),
     #用户管理
-    url(r'^vip/', views.vip, name='vip'),
+    url(r'^vip/(\d+)', views.vip, name='vip'),
     #话题管理
-    url(r'^topic/', views.topic, name='topic'),
-    #修改密码
-    url(r'^changepwd/', views.changepwd, name='changepwd'),
+    # url(r'^topic/', views.topic, name='topic'),
+    # #修改密码
+    # url(r'^changepwd/', views.changepwd, name='changepwd'),
     #删除用户，管理员等人
     url(r'^delvip/(\d+)', views.delvip, name='delvip'),
     #删除评论
@@ -49,5 +50,28 @@ urlpatterns = [
     url(r'^delmanager/(\d+)', views.delmanager, name='delmanager'),
     #删除电影
     url(r'^delmv/(\d+)', views.delmv, name='delmv'),
-
+    #轮播设置
+    url(r'^carousel/(\d+)', views.carousel, name='carousel'),
+    #vip观看设置
+    url(r'^viplook/(\d+)', views.viplook, name='viplook'),
+    #加入侧边栏设置
+    url(r'^sidebar/(\d+)', views.sidebar, name='sidebar'),
+    #假删除设置
+    url(r'^jiadel/(\d+)', views.jiadel, name='jiadel'),
+    #操作里面的真删除
+    url(r'^realdel/(\d+)', views.realdel, name='realdel'),
+    #添加管理员账户
+    url(r'^addmanager/(\d+)', views.addmanager, name='addmanager'),
+    #类别管理
+    url(r'^types/', views.types, name='types'),
+    #友情链接额
+    # url(r'^flink/', views.flink, name='flink'),
+    #用户充钱信息
+    url(r'^domoney/', views.domoney, name='domoney'),
+    #上传电影
+    # url(r'^addmv/', views.addmv, name='addmv'),
+    #删除电影类型
+    url(r'^deltype/(\d+)', views.deltype, name='deltype'),
+    #删除国家列表
+    url(r'^delct/(\d+)', views.delct, name='delct'),
 ]
